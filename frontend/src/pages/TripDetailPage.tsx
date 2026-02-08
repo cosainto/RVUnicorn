fce2d4b24e322ef8e7baca50708ed1ed27e11b33
William Roberts <roberts@Williams-MacBook-Pro.local>
2026-02-08 00:16:16 -0600

    Remove Edit Trip button

--- frontend/src/pages/TripDetailPage.tsx
+++ frontend/src/pages/TripDetailPage.tsx
@@ -727,7 +727,6 @@ export default function EventDetailPage() {
                           <Navigation className="w-4 h-4" />Open in Google Maps
                         </a>
                       )}
-                      
                     </div>
 
                     {/* Smart RV Stops */}
