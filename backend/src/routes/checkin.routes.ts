3cd16a6d5c830cebbbb945ee3143082816c2f42b
William Roberts <wroberts82@yahoo.com>
2026-03-21 17:49:05 -0500

    fix: include campground state in checkin response for StateVisit creation

--- backend/src/routes/checkin.routes.ts
+++ backend/src/routes/checkin.routes.ts
@@ -35,7 +35,7 @@ router.post('/', authenticateToken, async (req: any, res) => {
       },
       include: {
         user: { select: { id: true, username: true, firstName: true, lastName: true, profilePicture: true } },
-        campground: { select: { id: true, name: true, imageUrl: true } },
+        campground: { select: { id: true, name: true, imageUrl: true, state: true, location: true, latitude: true, longitude: true } },
       }
     });
 
